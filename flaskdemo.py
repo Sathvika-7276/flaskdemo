ba83a685b97adbc7bf53e89e506adad86fb80347
Sathvika-7284 <sathvika7276@gmail.com>
2023-01-22 21:55:18 +0800

    Wikipedia

--- flaskdemo.py
+++ flaskdemo.py
@@ -13,7 +13,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "I am still working on this"
+    return render_template("about.html")
 
 
 @app.route('/search', methods=['POST', 'GET'])
@@ -28,7 +28,8 @@ def search():
 def results():
     search_term = session['search_term']
     page = get_page(search_term)
-    return render_template("results.html", page=page)
+    page_titles = get_page(search_term)
+    return render_template("results.html", page=page, page_titles=page_titles)
 
 
 def get_page(search_term):
